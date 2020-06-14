e7570498aa66c751587034ab97ff11d5f75f6eac
Gabriel <rarokko@gmail.com>
2020-06-13 22:10:44 -0300

    Changed name

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Rarokko'
+export const siteTitle = 'Next.js Rarokko Sample Blog'
 
 export default function Layout({ children, home }) {
   return (
