3abb06e6799420ebf0574c5dd3fb709dd44986df
Gabriel <rarokko@gmail.com>
2020-06-14 04:18:19 -0300

    Dir fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { listUsers } from '../api/dynamo_scan'
+import { listUsers } from './api/dynamo_scan'
 // import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
